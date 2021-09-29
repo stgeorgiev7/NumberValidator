c6f48f639b6f6e71fcd260e2a5b83fc3ef2351bf
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-09-29 07:51:36 +0300

    added regex, useMemo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
+import React, {useState, useMemo} from "react";
 
 function App() {
+  const [text, setText] = useState('');
+  const checkIfValid = (string) => {
+    const numbers = /^\d+$/;
+    return numbers.test(string);
+  };
+
+  const validator = useMemo(() => checkIfValid(text));
+
+
   return (
     <div className="App">
      <div className="control has-icons-right">
@@ -8,10 +18,14 @@ function App() {
           className="input is-large"
           type="text"
           placeholder="Enter number..."
+          value={text}
+          onChange={(e)=> {
+            setText(e.target.value);
+          }}
 
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+          <i className={ validator? "fa-check" : "fas fa-times"} />
         </span> 
       </div>
     </div>
